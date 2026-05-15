4f0c3ca6338e07d9805e8e3983422a0e46babd35
mugdha-sarker81 <mugdhasarker81@gmail.com>
2026-05-15 11:22:10 +0600

    Implement missing number finder in C

--- final_exam_C_phitron/Find the Missing Number.c
+++ final_exam_C_phitron/Find the Missing Number.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
 
+       long long int m;
+       long long int a,b,c;
+       scanf("%lld %lld %lld %lld",&m,&a,&b,&c);
+
+      long long int min= a*b*c;
+       if(m%min==0){
+        printf("%lld\n",m/min);
+       }
+       else{
+        printf("-1\n");
+       }
+    } 
+    return 0;
+}
